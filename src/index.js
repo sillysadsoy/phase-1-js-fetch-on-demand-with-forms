ca6066f2b2b66218fea2d18550deaa8a4b4152ce
roger chat <mattrogerpelosi@gmail.com>
2021-08-17 12:10:31 -0400

    commit tree 630caf56f5dca68c4886c112f2acdc12d458cf04

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const input = document.querySelector('input#searchByID')
+    //   console.log(input.value);
+    //   form.reset();
+
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(resp => resp.json())
+      .then(movie => {
+          const title = document.querySelector('#movieDetails h4');
+          const summary = document.querySelector('#movieDetails p');
+          
+          title.textContent = movie.title;
+          summary.textContent = movie.summary;
+      })
+
+      form.reset();
+  });
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
